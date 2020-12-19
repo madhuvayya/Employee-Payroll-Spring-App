19ca2451fc331c9ccbcdd791c588f8213fe0330f
madhuvayya <madhu.vayya@gmail.com>
2020-12-19 21:25:56 +0530

    Added ResponseDTO class for creating the response object

---
+++ src/main/java/com/bridgelabz/employeepayroll/dto/ResponseDTO.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.employeepayroll.dto;
+
+public class ResponseDTO {
+	private String message;
+	private Object data;
+	
+	public ResponseDTO(String message, Object data) {
+		super();
+		this.message = message;
+		this.data = data;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public Object getData() {
+		return data;
+	}
+	
+	public void setData(Object data) {
+		this.data = data;
+	}	
+}
