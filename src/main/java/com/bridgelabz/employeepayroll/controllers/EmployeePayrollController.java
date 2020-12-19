d7f6094a03d8e38c43e1594eb710beb58676a60f
madhuvayya <madhu.vayya@gmail.com>
2020-12-19 20:55:22 +0530

    Added code to handle DELETE request for deleting employee data

--- src/main/java/com/bridgelabz/employeepayroll/controllers/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayroll/controllers/EmployeePayrollController.java
@@ -2,6 +2,7 @@ package com.bridgelabz.employeepayroll.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,5 +35,10 @@ public class EmployeePayrollController {
 	@PutMapping("/update")
 	public ResponseEntity<String> updateEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO) {
 		return new ResponseEntity<String>("Updated employee pay roll Data for:"+ employeePayrollDTO, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/delete/{empId}")
+	public ResponseEntity<String> deleteEmployeePayrollData(@PathVariable("empId") int empId) {
+		return new ResponseEntity<String>("Delete call success for id:"+ empId, HttpStatus.OK); 
 	}
 }
