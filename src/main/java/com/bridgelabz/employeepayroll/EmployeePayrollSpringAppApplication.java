56d34b32ec0b322807ba4dada2e9445a2a61b015
madhuvayya <madhu.vayya@gmail.com>
2020-12-19 17:03:23 +0530

    Initialized project setup

---
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/employeepayroll/EmployeePayrollSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
