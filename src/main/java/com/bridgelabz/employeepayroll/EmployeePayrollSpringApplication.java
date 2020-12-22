bc39d038c220f9862ef9fbb7968878db8c4e7ea6
madhuvayya <madhu.vayya@gmail.com>
2020-12-22 21:56:03 +0530

    Added logging pattern in appication.properties file

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollSpringApplication.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollSpringApplication.java
@@ -3,14 +3,17 @@ package com.bridgelabz.employeepayroll;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayrollSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollSpringApplication.class, args);
-		log.info("Employee Payroll App started");
+		ApplicationContext context= SpringApplication.run(EmployeePayrollSpringApplication.class, args);
+		log.info("Employee Payroll App started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
+		log.info(context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
