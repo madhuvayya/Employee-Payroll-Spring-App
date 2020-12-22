8d69b1abe96f04721ddc97df29031ec13efffb98
madhuvayya <madhu.vayya@gmail.com>
2020-12-22 20:11:33 +0530

    Added code for logging the application

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollSpringAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollSpringAppApplication.java
@@ -1,13 +1,16 @@
 package com.bridgelabz.employeepayroll;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollSpringAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollSpringAppApplication.class, args);
+		log.info("Employee Payroll App started");
 	}
 
 }
