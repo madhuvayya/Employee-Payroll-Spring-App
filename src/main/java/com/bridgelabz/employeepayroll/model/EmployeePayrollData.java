f1e38743d7affafc883a4523f20a2547efa40d96
madhuvayya <madhu.vayya@gmail.com>
2020-12-23 14:13:10 +0530

    Added annotations to map EmployeePayrollData to database

--- src/main/java/com/bridgelabz/employeepayroll/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayroll/model/EmployeePayrollData.java
@@ -3,18 +3,37 @@ package com.bridgelabz.employeepayroll.model;
 import com.bridgelabz.employeepayroll.dto.EmployeePayrollDTO;
 import lombok.Data;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@Entity
+@Table(name = "employee_payroll")
 public @Data class EmployeePayrollData {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "employee_id")
 	private int employeeId;
+
+	@Column(name = "name")
 	private String name;
+
 	private long salary;
+
 	private String gender;
+
+	@Column(name = "start_date")
 	private LocalDate startDate;
+
 	private String note;
+
+	@Column(name = "profile_pic")
 	private String profilePic;
+
+	@ElementCollection
+	@CollectionTable(name = "employee_department", joinColumns = @JoinColumn(name = "id"))
+	@Column(name = "department")
 	private List<String> department;
 	
 	public EmployeePayrollData() {}
