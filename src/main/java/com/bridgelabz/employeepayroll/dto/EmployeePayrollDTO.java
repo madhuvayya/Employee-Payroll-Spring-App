7bb66c69557cd1e24ec269ed016cb08c11f70da8
madhuvayya <madhu.vayya@gmail.com>
2020-12-23 11:06:41 +0530

    Added code to validate all the payroll DTO attributes

--- src/main/java/com/bridgelabz/employeepayroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayroll/dto/EmployeePayrollDTO.java
@@ -3,8 +3,7 @@ package com.bridgelabz.employeepayroll.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.ToString;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -16,15 +15,20 @@ public @ToString class EmployeePayrollDTO {
 	@Min(value = 500, message = "Min wage should be more than 500")
 	public long salary;
 
-	@Pattern(regexp = "male|female", message = "")
+	@Pattern(regexp = "male|female", message = "Gender needs to be male or female")
 	public String gender;
 
 	@JsonFormat(pattern = "dd MMM yyyy")
+	@NotNull(message = "Start date should not be empty")
+	@PastOrPresent(message = "Start date should be past or today's date")
 	public LocalDate startDate;
 
+	@NotBlank(message = "Note can't be empty")
 	public String note;
 
+	@NotBlank(message = "Profile pic can't be empty")
 	public String profilePic;
 
+	@NotNull(message = "Department should not be empty")
 	public List<String> department;
 }
--- src/main/java/com/bridgelabz/employeepayroll/exceptions/EmployeePayrollExceptionHandler.java
+++ src/main/java/com/bridgelabz/employeepayroll/exceptions/EmployeePayrollExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.employeepayroll.exceptions;
 
 import com.bridgelabz.employeepayroll.dto.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,7 +14,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Slf4j
 public class EmployeePayrollExceptionHandler {
+    private static final String message = "Exception while processing REST Request.";
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDTO> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception){
+        log.error("Invalid date formate:",exception);
+        ResponseDTO responseDTO=new ResponseDTO(message, "Should have date in the format dd MMM yyyy");
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
@@ -21,13 +32,13 @@ public class EmployeePayrollExceptionHandler {
                                         .stream()
                                         .map(mapper -> mapper.getDefaultMessage())
                                         .collect(Collectors.toList());
-        ResponseDTO responseDTO=new ResponseDTO("Exception while processing rest request", errMessage);
+        ResponseDTO responseDTO=new ResponseDTO(message, errMessage);
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EmployeePayrollException.class)
     public ResponseEntity<ResponseDTO> handleEmployeePayrollException(EmployeePayrollException exception){
-        ResponseDTO responseDTO = new ResponseDTO("Exception while processing rest request",
+        ResponseDTO responseDTO = new ResponseDTO(message,
                                                         exception.getMessage());
         return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
     }
